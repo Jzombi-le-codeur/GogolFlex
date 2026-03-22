8f64b722a334d5a2b76e8dec4954c04f12fba493
unknown <jzombijzjzombi2.0@gmail.com>
2026-03-22 14:47:08 +0100

    Added page informations in index.db

--- index.py
+++ index.py
@@ -6,12 +6,20 @@ import re
 
 class Indexer:
     def __init__(self):
-        self.page_informations = {"id": int(), "page_filename": str()}
+        self.page_informations = {"id": int(), "url": str(), "page_filename": str(), "title": str()}
         self.page_text = str()
         self.frequencies = dict()
 
     def init(self):
         db = sqlite3.connect("index.db")
+        db.execute("""
+        CREATE TABLE IF NOT EXISTS page_informations (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            url TEXT,
+            title TEXT,
+            total_words INTEGER
+        )
+        """)
         db.execute("""
         CREATE TABLE IF NOT EXISTS inverted_index (
             word TEXT,
@@ -26,8 +34,8 @@ class Indexer:
         # Get datas
         db = sqlite3.connect("parse.db")
         db_cursor = db.cursor()
-        db_cursor.execute("SELECT id, page_filename FROM page_informations ORDER BY id LIMIT 1")
-        self.page_informations["id"], self.page_informations["page_filename"] = db_cursor.fetchone()
+        db_cursor.execute("SELECT id, url, page_filename, title FROM page_informations ORDER BY id LIMIT 1")
+        self.page_informations["id"], self.page_informations["url"], self.page_informations["page_filename"], self.page_informations["title"] = db_cursor.fetchone()
         db.close()
 
         # Get page code
@@ -40,7 +48,7 @@ class Indexer:
 
     def __count_words(self):
         # Tokenize page text
-        self.page_text = re.sub(r"['\-,!?.]", " ", self.page_text)  # Remove some symbols from text
+        self.page_text = re.sub(r"['\-,!?.*]", " ", self.page_text)  # Remove some symbols from text
         tokens = self.page_text.split()  # Tokenize
         
         # Get word's frequencies in page
@@ -51,6 +59,17 @@ class Indexer:
 
             else:
                 self.frequencies[token] += 1
+
+    def __save_page_informations(self):
+        db = sqlite3.connect("index.db")
+        db.execute("INSERT INTO page_informations (id, url, title, total_words) VALUES (?, ?, ?, ?)", (
+            self.page_informations["id"],
+            self.page_informations["url"],
+            self.page_informations["title"],
+            len(self.frequencies.keys()),
+        ))
+        db.commit()
+        db.close()
 
     def __save_frequencies(self):
         db = sqlite3.connect("index.db")
@@ -64,10 +83,13 @@ class Indexer:
         db.commit()
         db.close()
 
+        print(sum(self.frequencies.values()))
+
     def run(self):
         self.init()
         self.__get_page_content()
         self.__count_words()
+        self.__save_page_informations()
         self.__save_frequencies()
 
 
