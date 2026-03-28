d4e4be7897083940371964c808f4f3cb48ff2298
unknown <jzombijzjzombi2.0@gmail.com>
2026-03-28 16:08:41 +0100

    Moved parser in parser.py

---
+++ parser.py
@@ -0,0 +1,152 @@
+import time
+from bs4 import BeautifulSoup
+import pathlib
+import sqlite3
+
+
+class Parser:
+    def __init__(self):
+        self.pages_informations = []  # {"id": int(), "url": str(), "page_filename": str(), "title": str()}
+        self.page_informations = {"id": int(), "url": str(), "page_filename": str(), "title": str()}
+        self.page_code = BeautifulSoup()
+
+    def init(self):
+        db = sqlite3.connect("parse.db")
+        db.execute("""
+        CREATE TABLE IF NOT EXISTS page_informations (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            url TEXT,
+            page_filename TEXT,
+            title TEXT,
+            indexed INTEGER
+        )
+        """)
+
+    def __get_crawl_results(self):
+        # Get visited_urls datas
+        db = sqlite3.connect("crawl.db")
+        db_cursor = db.cursor()
+        pages_informations = list()
+        while not pages_informations:
+            db_cursor.execute("SELECT id, url, indexation, page_filename FROM visited_urls WHERE parsed=0 ORDER BY id LIMIT 10")
+            pages_informations = db_cursor.fetchall()
+
+            for page_infos in pages_informations:
+                # Get informations
+                if bool(page_infos[2]):
+                    page_informations = {}
+                    page_informations["id"] = page_infos[0]
+                    page_informations["url"] = page_infos[1]
+                    page_informations["page_filename"] = page_infos[3]
+                    self.pages_informations.append(page_informations)
+
+                else:
+                    db.execute("UPDATE visited_urls SET parsed=1 WHERE id=?", (page_infos[0],))
+
+            if not pages_informations:
+                time.sleep(1)
+
+        db.close()
+
+    def __get_page_code(self):
+        # Get page code
+        page_filepath = pathlib.PurePath("Pages", self.page_informations["page_filename"][:2],
+                                         self.page_informations["page_filename"])
+        with open(page_filepath, encoding="utf-8") as page_file:
+            self.page_code = BeautifulSoup(page_file.read(), features="html.parser")
+
+    def __get_page_title(self):
+        # Find page title
+
+        # Try to get <title> tag
+        title = self.page_code.find("title").text.strip()
+        if title:
+            # Save title
+            self.page_informations["title"] = title
+
+        else:
+            # Try to get <meta property="og:title">
+            if not title:
+                title = self.page_code.find("meta", attrs={"property": "og:title"})
+
+            # Try to get <meta name="twitter:title">
+            if not title:
+                title = self.page_code.find("meta", name="twitter:title")
+
+            # Get meta title content
+            if title:
+                title = title["content"]
+
+            else:
+                # Try to get h1-6 tag
+                for i in range(1, 7):
+                    title = self.page_code.find(f"h{i}")
+                    if title:
+                        title = title.text
+
+            # Get title by URL
+            if not title:
+                title = self.page_informations["url"].split("/")[-1]
+                if "." in title:
+                    title = title.split(".")
+                    if len(title) == 2:
+                        title = title[0]
+
+                    else:
+                        title = ".".join(title[0:-1])
+
+        # Save URL in page_informations
+        self.page_informations["title"] = title.strip()
+
+    def __is_page_in_db(self):
+        db = sqlite3.connect("parse.db")
+        db_cursor = db.cursor()
+        db_cursor.execute("SELECT url FROM page_informations WHERE url = ?", (self.page_informations["url"],))
+        res = db_cursor.fetchone()
+        print(res)
+        return True if res else False
+
+    def __save_datas(self):
+        # Mark page as parsed
+        db = sqlite3.connect("crawl.db")
+        db.execute("UPDATE visited_urls SET parsed=1 WHERE id=?", (self.page_informations["id"],))
+        db.commit()
+        db.close()
+
+        # Save informations
+        if not self.__is_page_in_db():
+            db = sqlite3.connect("parse.db")
+            db.execute("INSERT INTO page_informations (url, page_filename, title, indexed) VALUES (?, ?, ?, ?)", (
+                self.page_informations["url"],
+                self.page_informations["page_filename"],
+                self.page_informations["title"],
+                0,
+            ))
+            db.commit()
+            db.close()
+
+        else:
+            print("DEJA DEDANS")
+
+    def run(self, i):
+        # Initialize database
+        self.init()
+
+        for _ in range(i):
+            # Get basic pages' information (url, index, pagepath)
+            if not self.pages_informations:
+                self.__get_crawl_results()
+
+            # Get page's informations
+            self.page_informations = self.pages_informations.pop(0)
+
+            print(self.page_informations)
+
+            # Get page code
+            self.__get_page_code()
+
+            # Get page title
+            self.__get_page_title()
+
+            # Save informations
+            self.__save_datas()
