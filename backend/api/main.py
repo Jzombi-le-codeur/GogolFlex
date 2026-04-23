b793ac0d3cf36a7a173b46773406613e8ab1767d
unknown <jzombijzjzombi2.0@gmail.com>
2026-04-23 21:24:44 +0200

    Added back-end API

---
+++ backend/api/main.py
@@ -0,0 +1,79 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from pydantic import BaseModel
+import psycopg
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class SearchRequest(BaseModel):
+    query: str
+    n_results: int
+
+@app.post("/search")
+def search(request: SearchRequest):
+    # Get query and numbers of results expected
+    query = request.query.split()
+    n_results = request.n_results
+
+    # Build SQL query
+    sql_query = ["SELECT page_id FROM inverted_index WHERE word = %s" for _ in query]
+    sql_query = " INTERSECT ".join(sql_query)
+
+    # Get results
+    db = psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="GogolFlexDB",
+        user="postgres",
+        password=os.getenv("PASSWORD"),
+    )
+    with db.cursor() as db_cursor:
+        if n_results != -1:
+            db_cursor.execute(f"""
+                SELECT inverted_index.url, inverted_index.title, 0.7*LOG(tf_idf + 1) + 0.3*LOG(page_rank + 1) AS score
+                FROM inverted_index JOIN page_informations USING (url)
+                WHERE word in ({', '.join([f"'{term}'" for term in query])})
+                AND page_id IN (
+                    {sql_query}
+                )
+                GROUP BY inverted_index.page_id, inverted_index.title, inverted_index.url, inverted_index.tf_idf, page_informations.page_rank
+                ORDER BY SUM(0.7*LOG(tf_idf + 1)) + 0.3*LOG(MAX(page_rank) + 1) DESC
+                LIMIT %s
+                """, tuple(query + [n_results])
+            )
+
+        else:
+            db_cursor.execute(f"""
+                SELECT inverted_index.url, inverted_index.title, 0.7*LOG(tf_idf + 1) + 0.3*LOG(page_rank + 1) AS score
+                FROM inverted_index JOIN page_informations USING (url)
+                WHERE word in ({', '.join([f"'{term}'" for term in query])})
+                AND page_id IN (
+                    {sql_query}
+                )
+                GROUP BY inverted_index.page_id, inverted_index.title, inverted_index.url, inverted_index.tf_idf, page_informations.page_rank
+                ORDER BY SUM(0.7*LOG(tf_idf + 1)) + 0.3*LOG(MAX(page_rank) + 1) DESC
+                """, tuple(query)
+            )
+
+        results = db_cursor.fetchall()
+
+    # Send results
+    results_ = [{"title": title, "url": url} for url, title, _ in results]
+    return {"results": results_}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
